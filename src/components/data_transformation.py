b9a7557d2adcfd9a3397525a6bfd15dc5d4ca02f
Zainab-ds <zainabriaz909@gmail.com>
2026-07-01 09:29:18 +0500

    Update transformation file

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,2 +1,35 @@
 import os
 import sys
+from dataclasses import dataclass
+import numpy as np 
+import pandas as pd 
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+@dataclass 
+class datatransformerconfig:
+    preprocessor_obj_file_path=os.path.join("artifacts","preprocessor.pkl")
+class Datatransformation:
+    def __init__(self):
+        self.data_transformation_config=datatransformerconfig()
+    def get_data_transformer_object(self):
+        try:
+            num_columns=["writing_score","reading_score"]
+            categorical_columns=[
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preprartion_score"
+            ]
+            num_pipeline=Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy="median"))
+                    ("scaler",StandardScaler())
+
+                ]
+            )
+        except:
